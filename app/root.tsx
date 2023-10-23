3f1fac6c315b97260794d09c49edb8b30728e21b
Vignesh Mohankumar <me@vig.xyz>
2023-10-23 10:46:17 -0400

    use client side routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import type { LinksFunction } from "@remix-run/node";
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -45,10 +46,10 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</a>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</a>
               </li>
             </ul>
           </nav>
