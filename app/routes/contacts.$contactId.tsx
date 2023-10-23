2531c6899e6e55f0a66b0f9b39f9557167da2381
Vignesh Mohankumar <me@vig.xyz>
2023-10-23 12:12:36 -0400

    typescipt fixes for contact

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -2,10 +2,15 @@ import { Form, useLoaderData } from "@remix-run/react";
 import type { FunctionComponent } from "react";
 
 import { getContact, type ContactRecord } from "../data";
-import { json } from "@remix-run/node";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import invariant from "tiny-invariant";
 
-export const loader = async ({ params }) => {
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId)
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
   return json({ contact })
 }
 
